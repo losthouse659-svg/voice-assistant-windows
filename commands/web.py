93074b93d290f4d9d4d578807de8c201fa038357
losthouse659-svg <losthouse659@gmail.com>
2026-06-28 20:40:11 +0200

    Oprava webu a pridani Comet prohlizece
    Refactor web URL opening functions and add shortcuts.

--- commands/web.py
+++ commands/web.py
@@ -1,35 +1,87 @@
 import webbrowser
-import json
+import subprocess
 import logging
 
 logger = logging.getLogger(__name__)
 
-def _check_whitelist(domain: str) -> bool:
+# Whitelist povolených domén
+WHITELIST = [
+    "youtube.com",
+    "google.com",
+    "google.cz",
+    "wikipedia.org",
+    "github.com",
+    "stackoverflow.com",
+    "reddit.com",
+    "facebook.com",
+    "twitter.com",
+    "instagram.com",
+    "twitch.tv",
+    "discord.com"
+]
+
+def open_website(query: str):
+    """Otevře webovou stránku na základě dotazu."""
+    query = query.lower().strip()
+    
+    # Zkratky pro populární weby
+    shortcuts = {
+        "youtube": "https://www.youtube.com",
+        "google": "https://www.google.com",
+        "wikipedi": "https://www.wikipedia.org",
+        "github": "https://github.com",
+        "stackoverflow": "https://stackoverflow.com",
+        "reddit": "https://www.reddit.com",
+        "facebook": "https://www.facebook.com",
+        "twitter": "https://twitter.com",
+        "instagram": "https://www.instagram.com",
+        "twitch": "https://www.twitch.tv",
+        "discord": "https://discord.com"
+    }
+    
+    # Zkontroluj zkratky
+    for key, url in shortcuts.items():
+        if key in query:
+            logger.info(f"Otevírám {url}")
+            webbrowser.open(url)
+            return True
+    
+    # Pokud obsahuje .cz, .com atd., otevři přímo
+    if "." in query:
+        url = query if query.startswith("http") else f"https://{query}"
+        logger.info(f"Otevírám URL: {url}")
+        webbrowser.open(url)
+        return True
+import webbrowser
+import subprocess
+import os
+import logging    # Jinak vyhledej na Googlu
+    search_url = f"https://www.google.com/search?q={query.replace(' ', '+')}"
+    logger.info(f"Vyhledávám na Googlu: {query}")
+    webbrowser.open(search_url)
+    return True
+
+def open_comet():
+    """Otevře prohlížeč Comet."""
     try:
-        with open("config/whitelist.json", "r", encoding="utf-8") as f:
-            whitelist = json.load(f)
-        allowed = whitelist.get("allowed_websites", [])
-        return any(d in domain for d in allowed)
-    except Exception:
+        # Zkus najít Comet v běžných umístěních
+        paths = [
+            r"C:\\Program Files\\Comet\\Comet.exe",
+            r"C:\\Program Files (x86)\\Comet\\Comet.exe",
+            r"C:\\Users\\{}\\AppData\\Local\\Comet\\Comet.exe".format(os.getenv('USERNAME'))
+        ]
+        
+        for path in paths:
+            if os.path.exists(path):
+                subprocess.Popen([path])
+                logger.info(f"Otevírám Comet: {path}")
+                return True
+        
+        # Pokud není nalezen, zkus otevřít přes start
+        subprocess.Popen(["start", "comet"], shell=True)
+        logger.info("Otevírám Comet přes start menu")
+        return True
+        
+    except Exception as e:
+        logger.error(f"Chyba při otevírání Comet: {e}")
         return False
-
-def open_url(url: str):
-    if not url.startswith("http"):
-        url = "https://" + url
-    if not _check_whitelist(url):
-        logger.warning(f"Web '{url}' neni na whitelistu.")
-        raise PermissionError(f"Web '{url}' neni povolen.")
-    webbrowser.open(url)
-    logger.info(f"Otevren web: {url}")
-
-def open_youtube():
-    webbrowser.open("https://www.youtube.com")
-
-def open_google():
-    webbrowser.open("https://www.google.com")
-
-def open_wikipedia():
-    webbrowser.open("https://cs.wikipedia.org")
-
-def open_github():
-    webbrowser.open("https://www.github.com")
